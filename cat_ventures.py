bf87c68362d7869d379969689fa34c1486e23311
Michael Duren <michaeld@michaelduren.com>
2022-07-04 13:55:00 -0500

    Refactored cv.py with new helper methods

--- cat_ventures.py
+++ cat_ventures.py
@@ -1,11 +1,8 @@
-import sys
+import sys, pygame
 
 from settings import Settings
 
 from cat import Cat
-
-import pygame
-
 class CatVentures:
     """Overall class to manage game assets and behavior."""
 
@@ -32,15 +29,25 @@ class CatVentures:
                 if event.type == pygame.QUIT:
                     sys.exit()
                 elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_RIGHT:
-                        self.cat.moving_right = True
-                    elif event.key == pygame.K_LEFT:
-                        self.cat.moving_left = True
+                    self._check_keydown_events(event)
                 elif event.type == pygame.KEYUP:
-                    if event.key == pygame.K_RIGHT:
-                        self.cat.moving_right = False
-                    elif event.key == pygame.K_LEFT:
-                        self.cat.moving_left = False
+                    self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        """Respond to keypresses."""
+        if event.key == pygame.K_RIGHT:
+            self.cat.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.cat.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+
+    def _check_keyup_events(self, event):
+        """Respond to key releases"""
+        if event.key == pygame.K_RIGHT:
+            self.cat.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.cat.moving_left = False
 
     def _update_screen(self):
         """Update images on the screen"""
