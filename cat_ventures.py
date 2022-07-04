83f07d1dca6a219c75362922647a928d794f4edf
Michael Duren <michaeld@michaelduren.com>
2022-07-04 11:18:03 -0500

    Added settings file
    Added settings.py, and eddited cat_ventures.py to import settings

--- cat_ventures.py
+++ cat_ventures.py
@@ -1,5 +1,7 @@
 import sys
 
+from settings import Settings
+
 import pygame
 
 class CatVentures:
@@ -8,8 +10,9 @@ class CatVentures:
     def __init__(self):
         """Initialize the game, and create game resources."""
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode(self.settings.screen_width, self.settings.screen_height)
         pygame.display.set_caption("Cat Ventures meow")
 
     def run_game(self):
@@ -19,6 +22,9 @@ class CatVentures:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+
+            # Redraw the screen during each pass through the loop.
+            self.screen.fill(self.settings.bg_color)
 
             # Make the most recently drawn screen visible.
             pygame.display.flip()
---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    """A Class to store all settings for Cat Ventures"""
+
+    def __init__(self):
+        """Initialize the games settings"""
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (255, 204, 255)
